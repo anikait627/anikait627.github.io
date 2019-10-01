f5dc1e677b0a462d19986110dc49aa368c97b14d
Anikait Sharma <anikaitsharma@yahoo.com>
2019-10-01 08:33:44 -0500

    index.html

---
+++ build/precache-manifest.26fe3040617e4ba70b5b08680fa8f035.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a3a0ac6c2eac5d181af30f7c570930",
+    "url": "/index.html"
+  },
+  {
+    "revision": "484406b0f65bd43a5055",
+    "url": "/static/css/main.90581d93.chunk.css"
+  },
+  {
+    "revision": "596cd5d95cd50f5e75c9",
+    "url": "/static/js/2.243fc2cd.chunk.js"
+  },
+  {
+    "revision": "484406b0f65bd43a5055",
+    "url": "/static/js/main.de62c714.chunk.js"
+  },
+  {
+    "revision": "0aca3f9ed8e709a37b28",
+    "url": "/static/js/runtime-main.5b8a2aae.js"
+  },
+  {
+    "revision": "581144c9b692254fc53aa539bb47cf2a",
+    "url": "/static/media/balloon.581144c9.jpg"
+  },
+  {
+    "revision": "7a4e9ca7ca1a7eb0a327cab53cac2f67",
+    "url": "/static/media/blue.7a4e9ca7.jpg"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/glyph-logo_May2016.0e84d592.png"
+  },
+  {
+    "revision": "e00bff5046580645e4f6f880ba35d917",
+    "url": "/static/media/me.e00bff50.png"
+  },
+  {
+    "revision": "b4eb86d7cc48432b4bfed0270ff9bd83",
+    "url": "/static/media/me.jpg.b4eb86d7.sb-018b27b0-Qla1hj"
+  },
+  {
+    "revision": "7434e26fd970d56de758ee75d5585577",
+    "url": "/static/media/mtn.7434e26f.jpg"
+  },
+  {
+    "revision": "b818d523d8560f33bcf5d6628f0ffa73",
+    "url": "/static/media/spotblack.b818d523.png"
+  }
+]);
