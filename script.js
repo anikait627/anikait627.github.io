c94cf5a39682b39f13f1ce1c2ea4c901715eeb98
Anikait Sharma <anikaitsharma@yahoo.com>
2019-11-11 15:33:17 -0600

    dark mode storage

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-$("#heart-beat").on('click', function() {
-    if($("body").hasClass("nightmode")){
-        $("body").removeClass("nightmode").css("transition", "1s all ease");
-        $(".nav-bar-nav.scrolled").css({"background-color": "white", "transition": "1s all ease"});
-        $(".nav-bar-link:hover").css("color", "#3581B8");
+$('body').toggleClass(localStorage.toggled);
+function darkLight() {
+    if (localStorage.toggled != 'nightmode') {
+       $('body').toggleClass('nightmode', true);
+       $(".nav-bar-nav.scrolled").css({"background-color": "rgb(80, 102, 128)", "transition": "1s all ease"});
+       localStorage.toggled = "nightmode";
     } else {
-        $("body").addClass("nightmode");
-        $(".nav-bar-nav.scrolled").css({"background-color": "rgb(80, 102, 128)", "transition": "1s all ease"});
-        $(".nav-bar-link:hover").css("color", "white");
+       $('body').toggleClass('nightmode', false).css("transition","1s all ease");
+       $(".nav-bar-nav.scrolled").css({"background-color": "white", "transition": "1s all ease"});
+       localStorage.toggled = "";
     }
-});
+ }
+
+// $('.nav-bar-nav.scrolled').toggleClass(localStorage.toggled);
+// function darkLight() {
+//     if (localStorage.toggled != 'nightmode-nav-bar') {
+//        $('.nav-bar-nav.scrolled').toggleClass('nightmode-nav-bar', true);
+//        localStorage.toggled = "nightmode-nav-bar";
+//     } else {
+//        $('body').toggleClass('nightmode-nav-bar', false).css("transition","1s all ease");
+//        localStorage.toggled = "";
+//     }
+//  }
