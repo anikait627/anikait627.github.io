d04ed624264bdbeaf47342e945b29526d4aae756
Anikait Sharma <anikaitsharma@yahoo.com>
2019-11-17 23:06:53 -0600

    nav-bar color

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $('body').toggleClass(localStorage.toggled);
 function darkLight() {
     if (localStorage.toggled != 'nightmode') {
        $('body').toggleClass('nightmode', true);
-       $(".nav-bar-nav.scrolled").css({"background-color": "rgb(80, 102, 128)", "transition": "1s all ease"});
+       $(".nav-bar-nav.scrolled").css({"background-color": "black", "transition": "1s all ease"});
        localStorage.toggled = "nightmode";
     } else {
        $('body').toggleClass('nightmode', false).css("transition","1s all ease");
